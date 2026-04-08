aaa0618b9a9cd677e9288ef39e1047d865dff8cf
VeronikaLia-1 <veronikalian@hotmail.com>
2026-04-08 12:18:11 +0200

    update yml file

--- BoardGameCafe/src/Api/obj/Debug/net10.0/api.AssemblyInfo.cs
+++ BoardGameCafe/src/Api/obj/Debug/net10.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e074acaabac3f5b9ba7bff0b5e4fb06f7b92414")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c07670b7b8ae1666dc10c439821ec059135f77c")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
